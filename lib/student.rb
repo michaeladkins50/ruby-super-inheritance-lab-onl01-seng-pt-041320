31fa8f2bda41649329f50e9bece97be784e2e5ba
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-15 00:24:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
   
   def hello
-    puts "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand
-    puts "pick me!\n"
+    puts "pick me!"
   end
   
 end

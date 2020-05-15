20d561fd2e60bb84ea34f0c7af51a70bea9388d2
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-15 00:22:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,11 @@
 class Student
-  attr_reader :hello, :raise_hand
   
-  Def hello
-    puts "Hey there! I'm so excited to learn stuff.\n"
+  def hello
+    "Hey there! I'm so excited to learn stuff.\n"
   end
   
   def raise_hand
-    puts "pick me!\n"
+    "pick me!\n"
   end
   
 end
